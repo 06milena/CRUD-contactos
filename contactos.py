3f020b16e0317ca8cd10d89b1c11af1eef19524c
Milena Tisoy <samitima02@gmail.com>
2021-09-10 11:52:37 -0500

    insertar y crear usuario

--- contactos.py
+++ contactos.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import cursor
 
 db= mysql.connector.connect(
     host='localhost',
@@ -7,3 +8,59 @@ db= mysql.connector.connect(
     database='blog',
     port=3306
 )
+
+def menuPrincipal():
+    opcion=1
+    
+    while opcion !=0:
+        print('---------------------------------')
+        print('Menu de contactos')
+        print('1. Crear contactos')
+        print('2. Listar contactos')
+        print('3. Actualizar contactos')
+        print('4. Eliminar contactos')
+        print('0. Salir')
+
+        opcion = int(input('SELECCIONE UNA OPCION: '))
+
+        if opcion==1:
+            crearUsuarios()
+        elif opcion==2:
+            listarUsuarios()
+        elif opcion==3:
+            actualizarUsuarios()
+
+            
+
+
+
+
+
+def crearUsuarios(nombre,email,contrasena):
+    nombre = input('Ingrese el nombre del contacto: ')
+    email = input('Ingrese el correo: ')
+    contrasena = input('Ingrese la contraseña: ')
+    cursor=db.cursor()
+    cursor.execute('''insert into 
+        usuarios(nombre,email,contrasena)
+        values(%s, %s, %s)''',(
+            nombre,
+            email,
+            contrasena
+        ))
+    print('ingresado')
+    db.commit()
+    cursor.close()
+
+crearUsuarios('nombre','email','contrasena')
+
+def listarUsuarios():
+    cursor=db.cursor()
+    cursor.execute('select * from usuarios')
+    usuarios=cursor.fetchall()
+    print(usuarios)
+
+#def actualizarUsuarios():
+
+
+menuPrincipal()
